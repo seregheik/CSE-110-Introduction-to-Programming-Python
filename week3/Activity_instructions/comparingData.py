0166ae1c1c6a131660821fe13d711a42b18c03be
El-Benson <122732891+El-Benson@users.noreply.github.com>
2023-05-03 15:19:02 +0100

    compairing numbers and strings

---
+++ week3/Activity_instructions/comparingData.py
@@ -0,0 +1,18 @@
+first_number = int(input('What is the first number? '))
+second_number = int(input('What is the second number? '))
+
+if first_number>second_number:
+    print('The first number is greater')
+else: print ('The first number is not greater')
+if first_number == second_number:
+    print('The numbers are equal')
+else : print('The numbers are not equal')
+if first_number < second_number:
+    print('The second number is greater')
+else : print('The second number is not greater')
+
+my_fav= 'cats'
+your_fav = input('\nWhat is your favourite animal? ').lower()
+if my_fav == your_fav:
+    print('That\'s my favorite animal too!')
+else: print('That one is not my favorite.')
