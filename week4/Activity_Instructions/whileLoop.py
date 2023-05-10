f3dfa646c218ec38e5bf5de64db27df89503f811
El-Benson <122732891+El-Benson@users.noreply.github.com>
2023-05-10 17:33:19 +0100

    learning for loops

--- week4/Activity_Instructions/whileLoop.py
+++ week4/Activity_Instructions/whileLoop.py
@@ -1,11 +1,11 @@
-# number = int(input('Please type a positive number: '))
+number = int(input('Please type a positive number: '))
 
-# while number<0:
-#     print('Sorry, that is a negative number. Please try again.')
-#     number = int(input('Please type a positive number: '))
-# print(f'The number is: {number}')
+while number<0:
+    print('Sorry, that is a negative number. Please try again.')
+    number = int(input('Please type a positive number: '))
+print(f'The number is: {number}')
 
-# answer = input('May I have a piece of candy? ').lower()
+answer = input('May I have a piece of candy? ').lower()
 
 
 answer = False
