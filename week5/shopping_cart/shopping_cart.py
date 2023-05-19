8ece58300d79d65a392b86cbf2115992197ede02
El-Benson <122732891+El-Benson@users.noreply.github.com>
2023-05-19 18:46:19 +0100

    Finally done with this cart

--- week5/shopping_cart/shopping_cart.py
+++ week5/shopping_cart/shopping_cart.py
@@ -1,5 +1,10 @@
+# Author: Osasere Ikponmwosa 
+
+# for creativity I made use of error handling should the user enter an unexpected input
+
 actions = ['Add item', 'View Cart', 'Remove item', 'Compute total', 'Quit']
 shopping_cart = []
+items_prices = []
 
 print('Welcome to the shopping cart program!')
 end = False
@@ -20,21 +25,25 @@ while not end:
             print('Thank you. Goodbye.')
             end = True
             break
+    # this action computes the total price of items in the cart
         elif user_action == 3:
-            print('to be done')
+            sum = 0
+            for index in range(len(items_prices)):
+                to_be_summed = items_prices[index]
+                sum = to_be_summed + sum
+            print(f'The total price of the items in the shopping cart is ${sum:.2f}')
         
     # this action is for user to remove item from shopping list
         elif user_action == 2:
             remove_from_cart_string = input('What item would you like to remove? ')
-
     # this 'try' is to handle errors resulting from invalid user input
             try:
                 remove_from_cart = int(remove_from_cart_string)
-                shopping_cart.pop(remove_from_cart)
+                shopping_cart.pop(remove_from_cart-1)
                 print('Item removed')
     # the error value if user enters wrong input for removal of item
             except ValueError:
-                print('Please enter a valid input')
+                print('!!!Please enter a valid input!!!')
 
     # this action is for user to view contents of shopping cart   
         elif user_action == 1:
@@ -44,15 +53,19 @@ while not end:
                 print('\nThe contents of the shopping cart are:')
                 for j in range(len(shopping_cart)):
                     cart_items = shopping_cart[j]
-                    print(f'{j+1}. {cart_items}')
-
-
+                    price = items_prices[j]
+                    print(f'{j+1}. {cart_items} - ${price:.2f}')
     # this action is for user to add items to cart
         elif user_action == 0:
             add_to_cart = input('What item would you like to add? ').capitalize()
-
+            price_of_item = input(f"What is the price of '{add_to_cart}'? ")
+            try:
+                item_price = float(price_of_item)
     # I made use of double quotations here because of the single quotation in the centence
-            print(f"'{add_to_cart}' has been added to the cart")
-            shopping_cart.append(add_to_cart)
+                print(f"'{add_to_cart}' has been added to the cart")
+                shopping_cart.append(add_to_cart)
+                items_prices.append(item_price)
+            except ValueError:
+                print('\n!!!Please enter a proper price!!!')
     except ValueError:
         print('\n!!! Please type in a proper value !!!')
