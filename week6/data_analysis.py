8403d8084d0816fb0adc3c97067f1b9cb3c837c9
Osasere Ik <edenosas1@gmail.com>
2023-06-01 12:13:31 +0100

    finally done

--- week6/data_analysis.py
+++ week6/data_analysis.py
@@ -16,7 +16,6 @@ elif user_input == '2' or user_input == 'Country':
     country_of_interest = input('Enter country of interest: ')
     if len(country_of_interest) == 3:
         country_of_interest = country_of_interest.upper()
-# country_of_interest = 'USA'
 drop = 0
 
 with open('./life-expectancy.csv') as life_expectancy:
@@ -29,6 +28,7 @@ with open('./life-expectancy.csv') as life_expectancy:
         code = life_expectancy[1]
         year = int(life_expectancy[2])
         expectancy = float(life_expectancy[3])
+        # the next set if codes calculate for the maximum and minimum of the entire data
         if expectancy >= overall_max_life_expectancy:
             overall_max_life_expectancy = expectancy
             overall_max_entity = entity
@@ -37,6 +37,7 @@ with open('./life-expectancy.csv') as life_expectancy:
             overall_min_life_expectancy = expectancy
             overall_min_entity = entity
             overall_min_year = year
+        # this is for calculating if user selects country
         if user_input == 'Country':
             if entity == country_of_interest or country_of_interest == code:
                 country_of_interest = entity
@@ -48,6 +49,7 @@ with open('./life-expectancy.csv') as life_expectancy:
                 summation += expectancy
                 reiteration += 1
                 average_expectancy = summation/reiteration
+        # this is for calculating if user selects year
         elif user_input == 'Year':
             if year == year_of_interest:
                 if expectancy >= max_life_expectancy:
@@ -59,14 +61,15 @@ with open('./life-expectancy.csv') as life_expectancy:
                 summation += expectancy
                 reiteration += 1
                 average_expectancy = summation/reiteration
+
+print(f'\nThe overall max life expectancy is: {overall_max_life_expectancy} from {overall_max_entity} in {overall_max_year}')
+print(f'The overall min life expectancy is: {overall_min_life_expectancy} from {overall_min_entity} in {overall_min_year}')
 if user_input == 'Year':
-    print(f'\nThe overall max life expectancy is: {overall_max_life_expectancy} from {overall_max_entity} in {overall_max_year}')
-    print(f'The overall min life expectancy is: {overall_min_life_expectancy} from {overall_min_entity} in {overall_min_year}')
     print(f'\nFor the year {year_of_interest}:')
     print(f'The average life expectancy accross all countries was {average_expectancy:.2f}')    
     print(f'The max life expectancy was in {max_entity} with {max_life_expectancy}')       
     print(f'The min life expectancy was in {min_entity} with {min_life_expectancy}')
 if user_input =='Country': 
-    print(f'The maximum life expectancy for {country_of_interest} is {max_life_expectancy}')
+    print(f'\nThe maximum life expectancy for {country_of_interest} is {max_life_expectancy}')
     print(f'The minimum life expectancy for {country_of_interest} is {min_life_expectancy}')
     print(f'The average life expectancy for {country_of_interest} is {average_expectancy: .2f}')
